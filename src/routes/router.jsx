32aba5cfba144b46e8f00cf459d9444e7687deba
ismail <coderismail22@gmail.com>
2024-04-01 17:43:13 +0600

    install-redux-toolkit

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Home } from "../pages/home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
   },
 ]);
